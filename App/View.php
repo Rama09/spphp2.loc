3e8f0b6b0dbd7202619740a13951be9d5eaacda0
Roman <kefirr09@yandex.ru>
2016-12-29 11:30:16 +0300

    до

---
+++ App/View.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roman
+ * Date: 25.11.16
+ * Time: 11:45
+ */
+
+namespace App;
+
+
+class View
+{
+
+}
---
+++ App/templates/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roman
+ * Date: 23.11.16
+ * Time: 13:41
+ */
